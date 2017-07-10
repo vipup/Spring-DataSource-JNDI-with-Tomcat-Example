8ac1ec7f811436c32f3c81ed65e583e8c637727a
Va Sja <VaSja0441@gMaIl.com>
2017-07-10 15:49:32 +0200

    http://localhost:8080/HibernateDataSource-1.0.0-BUILD-SNAPSHOT//rest/emps
    [{"id":1,"name":"Pankaj","role":"CEO","insertTime":null},{"id":2,"name":"David","role":"Manager","insertTime":null}]

--- src/main/java/com/journaldev/spring/jdbc/controller/EmployeeController.java
+++ src/main/java/com/journaldev/spring/jdbc/controller/EmployeeController.java
@@ -1,16 +1,14 @@
 package com.journaldev.spring.jdbc.controller;
+ 
+import java.util.List; 
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import javax.sql.DataSource;
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;  
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.slf4j.LoggerFactory;  
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,32 +23,21 @@ import com.journaldev.spring.jdbc.model.Employee;
 public class EmployeeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
-	
-	@Autowired
-	@Qualifier("dbDataSource")
-	private DataSource dataSource;
-
-	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
-	}
-
+	 
 	@RequestMapping(value = "/rest/emps", method = RequestMethod.GET)
-	public @ResponseBody List<Employee> getAllEmployees() {
+	public @ResponseBody List<Employee> getAllEmployees(
+			HttpServletRequest request,
+			HttpServletResponse response			
+			) {
 		logger.info("Start getAllEmployees.");
-		List<Employee> empList = new ArrayList<Employee>();
-		//JDBC Code - Start
-		String query = "select id, name, role from Employee";
-		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-
-		List<Map<String,Object>> empRows = jdbcTemplate.queryForList(query);
 		
-		for(Map<String,Object> empRow : empRows){
-			Employee emp = new Employee();
-			emp.setId(Integer.parseInt(String.valueOf(empRow.get("id"))));
-			emp.setName(String.valueOf(empRow.get("name")));
-			emp.setRole(String.valueOf(empRow.get("role")));
-			empList.add(emp);
-		}
+		SessionFactory sessionFactory = (SessionFactory) request.getServletContext().getAttribute("SessionFactory");
+		Session session = sessionFactory.getCurrentSession();
+		
+		Transaction tx = session.beginTransaction();
+		List<Employee> empList =  session.createCriteria(Employee.class).list();
+		tx.commit();
+		 
 		
 		return empList;
 	}
